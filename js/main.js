44d7781ab961ee3f4025ab419dbbd42fa74180e3
lloydbutay123 <johnlloydbutay123@gmail.com>
2024-02-22 14:50:23 +0800

    arrow added

--- js/main.js
+++ js/main.js
@@ -1,16 +1,48 @@
 $(document).ready(function(){
+    var current_img;
     
     $(".img-fluid").on("click", function(){
         var src = $(this).attr("src");
+        current_img = $(this).parent();
         $("#frame img").attr("src", src);
         $("#overlay").fadeIn();
+        $(".button-container").fadeIn();
         $("#frame").fadeIn();
+        $(".right").fadeIn();
+        $(".left").fadeIn();
 
     });
 
-    $("#overlay").on("click", function(){
+    $(".fa-xmark").on("click", function() {
         $("#frame").fadeOut();
         $("#overlay").fadeOut();
+        $(".button-container").fadeOut();
+        $(".right").fadeOut();
+        $(".left").fadeOut();
+    })
+
+
+    $(".right").on("click", function(){
+
+        if(current_img.is(":last-child")) {
+            var next_img = $("#frame img").first();
+
+        } else { 
+            var next_img = current_img.next();
+        }
+
+        var next_src = next_img.children("img").attr("src");
+        $("#frame img").attr("src", next_src);
+        current_img = next_img;
+
+    });
+
+    $(".left").on("click", function() {
+        var pre_img = current_img.prev();
+        var pre_src = pre_img.children("img").attr("src");
+        $("#frame img").attr("src", pre_src);
+        current_img = pre_img;
+
     })
     
 });
